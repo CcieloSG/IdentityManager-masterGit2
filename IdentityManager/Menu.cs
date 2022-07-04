29639b4d7c0177b438a4e187a2d942dd8bae0fed
Loand Corona Saenz <cielosagr5@gmail.com>
2022-07-04 01:28:44 -0500

    Este es el menu de mi app web

--- IdentityManager/Menu.cs
+++ IdentityManager/Menu.cs
@@ -1,2 +1,20 @@
-Este es el menu de mi app web
-Add Menu 
+<html>
+  <body>
+    <header>
+      <nav>
+        <ul id="menu">
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Plantas</a>
+            <ul>
+              <li><a href="">Descripcion</a></li>
+              <li><a href="">Costo</a></li>
+              <li><a href="">Carrito</a></li>
+            </ul>  
+          </li>
+          <li><a href="">Tipos de pago</a></li>
+          <li><a href="">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
