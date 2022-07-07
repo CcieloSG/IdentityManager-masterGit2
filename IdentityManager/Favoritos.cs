bcc6112616dc06a28361ad18e7f396d79d0360a6
Loand Corona Saenz <cielosagr5@gmail.com>
2022-07-06 23:43:32 -0500

    Desplegable de las plantas favoritas

---
+++ IdentityManager/Favoritos.cs
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Favoritos</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+</head>
+<body>
+<div id="contenedor">
+	<h2>Mis Plantas favoritas</h2>
+	<form id="formulario1" name="formulario1" method="post" action="data.php">
+	<h3>Selecciona tus plantas foritas</h3>
+		<select name="peliculas" id="peliculas" multiple="multiple" size="9">	
+			<option value="1">Rosales</option>
+			<option value="2" disabled="disabled">Suculentas</option>
+			<option value="3">Arbustos</option>
+			<option value="4">Arboles</option>
+			<option value="5">Matas</option>
+			<option value="6">Llervas</option>
+			<option value="7">Plantas acuaticas</option>
+	</form>
+</div>	
+</body>
+</html>
