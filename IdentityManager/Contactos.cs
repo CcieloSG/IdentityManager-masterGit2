7e1737c83c2084cd745be8cabd5eacbad0249963
Loand Corona Saenz <cielosagr5@gmail.com>
2022-07-08 00:59:09 -0500

    Archivo Contactos.cs modificado

--- IdentityManager/Contactos.cs
+++ IdentityManager/Contactos.cs
@@ -1,2 +1,36 @@
-Este permitira ver los contactos del Vivero Corenz
-Tambien las direcciones 
+<html>
+<head>
+	<title>Desplegable de Provedores</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8">
+</head>
+<body>
+<div id="contenedor">
+	<h2 id="encabezado">Provedores</h2>
+	<form id="formulario1" name="formulario1" method="post">
+		<h3>Provedor de plantas</h3>
+		<input type="id" name="nombre" value="1">Rosales<br>
+		<input type="id" name="nombre" value="2">Suculentas<br>
+        <input type="id" name="nombre" value="3">Arbustos<br>
+        <input type="id" name="nombre" value="4">Arboles<br>
+        <input type="id" name="nombre" value="5">Matas<br>
+        <input type="id" name="nombre" value="6">Llervas<br>
+        <input type="id" name="nombre" value="7">Plantas acuaticas<br>
+
+        <h3>Provedor de abono y fertilizante</h3>
+		<input type="id" name="nombre" value="1">Abono universal<br>
+        <input type="id" name="nombre" value="2">Abonopara rosales<br>
+        <input type="id" name="nombre" value="3">Abono organico<br>
+        <input type="id" name="nombre" value="4">Abono minerales<br>
+        <input type="id" name="nombre" value="5">Abono para plantasverdes<br>
+        <input type="id" name="nombre" value="6">Abono para citricos<br>
+        <input type="id" name="nombre" value="7">Fertilizantes organicos<br>
+        <input type="id" name="nombre" value="1">Fertilizantes quimico<br>
+        <input type="id" name="nombre" value="1">Biofertilizante<br>
+
+        <h3>Provedor de tierra</h3>
+		<input type="id" name="nombre" value="1">Tierra lama<br>
+        <input type="id" name="nombre" value="1">Tierra con hojarasca<br>
+	</form>
+</div>	
+</body>
+</html>
