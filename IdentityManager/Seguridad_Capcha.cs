3dc460166444ba0b6d61d51be700ca52ab32811a
Loand Corona Saenz <cielosagr5@gmail.com>
2022-07-09 01:19:01 -0500

    Metodo de seguridad

---
+++ IdentityManager/Seguridad_Capcha.cs
@@ -0,0 +1,36 @@
+namespace Application\Form;
+// ...
+
+class ContactForm extends Form
+{
+    // ...
+    protected function addElements()
+    {
+        // ...
+
+        // Add the CAPTCHA field
+        $this->add([
+            'type'  => 'captcha',
+            'name' => 'captcha',
+            'attributes' => [
+            ],
+            'options' => [
+                'label' => 'Human check',
+                'captcha' => [
+                    'class' => 'Image',
+                    'imgDir' => 'public/img/captcha',
+                    'suffix' => '.png',
+                    'imgUrl' => '/img/captcha/',
+                    'imgAlt' => 'CAPTCHA Image',
+                    'font'   => './data/font/thorne_shaded.ttf',
+                    'fsize'  => 24,
+                    'width'  => 350,
+                    'height' => 100,
+                    'expiration' => 600,
+                    'dotNoiseLevel' => 40,
+                    'lineNoiseLevel' => 3
+                ],
+            ],
+        ]);
+    }
+}
